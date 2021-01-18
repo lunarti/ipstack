ffae412da416c5867f1a585884890240b8714f0a
lunarti <lunarti@gmail.com>
2021-01-18 14:26:27 -0300

    ipstackaxiosv1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card text-center m-3">
+    <h5 class="card-header">GET Request Ipstack Axios Vue</h5>
+    <div class="card-body">Geo Info: {{GeoInfo}}</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "get-request",
+  data() {
+    return {
+      GeoInfo: null,
+      errorMessage: null
+    };
+  },
+  created() {
+    // Simple GET request using axios 
+    axios.get('http://api.ipstack.com/189.37.65.210?access_key=2b5361b17468b3dc0664c269703cc043')
+      .then(response => this.GeoInfo = response)
+      .catch(error => {
+        this.errorMessage = error.message;
+        console.error("There was an error!", error);
+  });
 }
+};
 </script>
 
 <style>
